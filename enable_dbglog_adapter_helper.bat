3405fa9182cca0443efb8a60bd078bb1d63de455
james_cm_lee <james_cm_lee@trendmicro.com>
2024-09-19 18:05:12 +0800

    add helper log related bat

---
+++ enable_dbglog_adapter_helper.bat
@@ -0,0 +1,11 @@
+:: Check for administrative privileges
+net session >nul 2>&1
+if %errorLevel% NEQ 0 (
+    echo This script requires administrative privileges.
+    pause
+    exit /b 1
+)
+cd /d %~dp0
+
+reg add HKLM\SOFTWARE\TrendMicro\UMH /v DebugLogFlags /t REG_DWORD /d 1 /f /reg:64
+pause
