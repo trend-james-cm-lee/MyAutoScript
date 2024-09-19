02d35fade78cfeb964f5aa3f3191c46d8f4cb255
james_cm_lee <james_cm_lee@trendmicro.com>
2024-09-19 18:05:39 +0800

    add BSOD loop prevention related bat

---
+++ BsodLoopPrevention/Disable_BsodLoopPrevention.bat
@@ -0,0 +1,31 @@
+@echo off
+setlocal
+
+:: Check for administrative privileges
+net session >nul 2>&1
+if %errorLevel% NEQ 0 (
+    echo This script requires administrative privileges.
+    pause
+    exit /b 1
+)
+
+echo Stop the service tmumh
+sc stop tmumh
+echo Query the service tmumh
+sc query tmumh
+
+set BsodLoopPreventionSwitch="GGCS"
+set BsodLoopPreventionSwitchValue=0
+
+echo.
+echo Set the registry key %BsodLoopPreventionSwitch% to %BsodLoopPreventionSwitchValue%
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\tmumh\Parameters" /f /v %BsodLoopPreventionSwitch% /t REG_DWORD /d %BsodLoopPreventionSwitchValue%
+echo Done
+
+echo.
+echo Query the registry key %BsodLoopPreventionSwitch%
+reg query "HKLM\SYSTEM\CurrentControlSet\Services\tmumh\Parameters" /v %BsodLoopPreventionSwitch%
+echo Done
+
+endlocal
+pause
---
+++ BsodLoopPrevention/Enable_BsodLoopPrevention_Loose_Threshold.bat
@@ -0,0 +1,43 @@
+@echo off
+setlocal
+
+:: Check for administrative privileges
+net session >nul 2>&1
+if %errorLevel% NEQ 0 (
+    echo This script requires administrative privileges.
+    pause
+    exit /b 1
+)
+
+echo Stop the service tmumh
+sc stop tmumh
+echo Query the service tmumh
+sc query tmumh
+
+set BsodLoopPreventionSwitch="GGCS"
+set BsodLoopPreventionSwitchValue=1
+set FailureCountThreshold="GGCT"
+set FailureCountThresholdValue=16
+set AutoResetSecond="ALSN"
+set AutoResetSecondValue=300
+
+echo.
+echo Set the registry key value %BsodLoopPreventionSwitch% to %BsodLoopPreventionSwitchValue%
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\tmumh\Parameters" /f /v %BsodLoopPreventionSwitch% /t REG_DWORD /d %BsodLoopPreventionSwitchValue%
+echo Set the registry key value %FailureCountThreshold% to %FailureCountThresholdValue%
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\tmumh\Parameters" /f /v %FailureCountThreshold% /t REG_DWORD /d %FailureCountThresholdValue%
+echo Set the registry key value %AutoResetSecond% to %AutoResetSecondValue%
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\tmumh\Parameters" /f /v %AutoResetSecond% /t REG_DWORD /d %AutoResetSecondValue%
+echo Done
+
+echo.
+echo Query the registry key value %BsodLoopPreventionSwitch%
+reg query "HKLM\SYSTEM\CurrentControlSet\Services\tmumh\Parameters" /v %BsodLoopPreventionSwitch%
+echo Query the registry key value %FailureCountThreshold%
+reg query "HKLM\SYSTEM\CurrentControlSet\Services\tmumh\Parameters" /v %FailureCountThreshold%
+echo Query the registry key value %AutoResetSecond%
+reg query "HKLM\SYSTEM\CurrentControlSet\Services\tmumh\Parameters" /v %AutoResetSecond%
+echo Done
+
+endlocal
+pause
