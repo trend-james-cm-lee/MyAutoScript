8d35e0e83a8b67169af2da88eff8a5e1aad3133e
james_cm_lee <james_cm_lee@trendmicro.com>
2024-10-02 16:59:37 +0800

    add DisableNtTestAlertCheck.bat

---
+++ DisableNtTestAlertCheck/DisableNtTestAlertCheck.bat
@@ -0,0 +1,42 @@
+@echo off
+setlocal
+
+:: Check for administrative privileges
+net session >nul 2>&1
+if %errorLevel% NEQ 0 (
+    echo This script requires administrative privileges.
+    pause
+    exit /b 1
+)
+
+echo Stop the service tmumh
+sc stop tmumh
+echo Query the service tmumh
+sc query tmumh
+
+HKLM\SYSTEM\CurrentControlSet\Services\tmumh\RegCfgFlags REG_DWORD 1
+
+set DisableNtTestAlertCheck=1
+
+echo.
+echo Set the registry key RegCfgFlags to %DisableNtTestAlertCheck%
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\tmumh" /v "RegCfgFlags" /t REG_DWORD /d %DisableNtTestAlertCheck% /f
+if %errorLevel% NEQ 0 (
+    echo Failed to set the registry key RegCfgFlags
+    pause
+    exit /b 1
+)
+echo Done
+
+echo.
+echo Query the registry key RegCfgFlags
+reg query "HKLM\SYSTEM\CurrentControlSet\Services\tmumh" /v "RegCfgFlags"
+if %errorLevel% NEQ 0 (
+    echo Failed to query the registry key RegCfgFlags
+    pause
+    exit /b 1
+)
+echo Done
+
+endlocal
+pause
