5b239dd739e2665883833a7dc9b298aa9fcfb03b
james_cm_lee <james_cm_lee@trendmicro.com>
2024-10-09 17:23:59 +0800

    add script for testUMH

---
+++ testUMH/end_testUMH.bat
@@ -0,0 +1,34 @@
+@echo off
+setlocal
+
+:: Check for administrative privileges
+net session >nul 2>&1
+if %errorLevel% NEQ 0 (
+    echo This script requires administrative privileges.
+    pause
+    exit /b 1
+)
+
+set TESTUMH_DIR=c:\users\%username%\testUMH
+
+if not exist "%TESTUMH_DIR%" (
+    echo %TESTUMH_DIR% not found
+    pause
+    exit /b 1
+)
+
+cd /d %TESTUMH_DIR%
+echo Running the script from %cd%
+
+echo testUmhLoader.exe /stopdriver
+testUmhLoader.exe /stopdriver
+if %errorLevel% NEQ 0 (
+    echo Failed to run testUmhLoader.exe
+    pause
+    exit /b 1
+)
+
+echo Done
+
+endlocal
+pause
